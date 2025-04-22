2f56a6a6607509bcf916d14161f3aa677e7e1527
dsolerh <dsolerh.cinter95@gmail.com>
2025-04-23 00:38:44 +0300

    "ci: publish packages utils,publisher"

---
+++ publisher/workparser.go
@@ -0,0 +1,40 @@
+package publisher
+
+import (
+	"os"
+	"strings"
+
+	"golang.org/x/mod/modfile"
+)
+
+func ParseGoWorkFile(path string) (*modfile.WorkFile, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	// Parse the go.work file
+	return modfile.ParseWork(path, data, nil)
+}
+
+type PackageInfo struct {
+	OldPath string // the path the package has in the current project structure
+	NewPath string // the package name and new path
+	Version string // the package current version
+}
+
+type PackagesMap map[string]*PackageInfo
+
+func MakePackagesMap(work *modfile.WorkFile) PackagesMap {
+	pmap := make(PackagesMap, len(work.Replace))
+	for _, replace := range work.Replace {
+		parts := strings.Split(replace.New.Path, "/")
+		pkey := parts[len(parts)-1]
+		pmap[pkey] = &PackageInfo{
+			OldPath: replace.Old.Path,
+			NewPath: replace.New.Path,
+			Version: replace.New.Version,
+		}
+	}
+	return pmap
+}
