6302d83afd2fe83074b595de03a33857ac6aaadc
dsolerh <dsolerh.cinter95@gmail.com>
2025-04-23 02:15:38 +0300

    "ci: cleanup publish"

--- publisher/version.go
+++
@@ -1,3 +0,0 @@
-package publisher
-
-func Version() string { return "0.1.0" }
--- utils/utils.go
+++
@@ -1,5 +0,0 @@
-package utils
-
-func Version() string { return "0.1.0" }
-
-func SubVersion() string { return "0.1.0" }
