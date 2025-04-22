f90c7db21d814d0a0138d3d9610759b4fbffac3a
dsolerh <dsolerh.cinter95@gmail.com>
2025-04-23 00:50:55 +0300

    chore: update

--- packages/core/foo/foo.go
+++ packages/core/foo/foo.go
@@ -1,3 +1,11 @@
 package foo
 
-func Version() string { return "0.0.2" }
+import (
+	"fmt"
+
+	"github.com/dsolerh/go-test-mono/packages/utils"
+)
+
+func Version() string {
+	return fmt.Sprintf("own: 0.0.3, depends of utils: %s", utils.Version())
+}
